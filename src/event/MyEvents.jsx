36f4b6a0c22c01493da289eea11d83b6fd16610d
DGRamya <ramyshreedg@gmail.com>
2018-11-12 15:01:43 -0500

    Adding header

--- src/event/MyEvents.jsx
+++ src/event/MyEvents.jsx
@@ -6,44 +6,50 @@ import Alert from "react-s-alert";
 class MyEvents extends Component {
   constructor(props) {
     super(props);
-     this.state = {
-      eventList : '',
-      str: ''
+    this.state = {
+      eventList: "",
+      str: ""
     };
     this.state.str = [
-      {"eventname":"Test Event","venue":"IMU"}, {"eventname":"Thanksgiving","venue":"IMU"}
-     ];
-     this.fetchEvents = this.fetchEvents.bind(this);
+      { eventname: "Test Event", venue: "IMU" },
+      { eventname: "Thanksgiving", venue: "IMU" }
+    ];
+    this.fetchEvents = this.fetchEvents.bind(this);
   }
 
-componentDidMount() {
+  componentDidMount() {
     this.fetchEvents();
-}
+  }
 
- fetchEvents() {
-  getMyEvents()
-    .then(response => {
-      console.log("response found :: " + JSON.stringify(response));
-      this.setState({
-        str: response.events
+  fetchEvents() {
+    getMyEvents()
+      .then(response => {
+        this.setState({
+          str: response.events
+        });
+      })
+      .catch(error => {
+        Alert.error("Oops! Something went wrong. Please try again!");
       });
-    })
-    .catch(error => {
-      Alert.error("Oops! Something went wrong. Please try again!");
-    });
-}
+  }
 
-render() {
+  render() {
     return (
-      <div>{
-        this.state.str.map((event, i) => {
-          return <Jumbotron parentMethod={this.fetchEvents} mainText={event.eventName}
-          subText={event.venue}
-          displayState="true" eventId = {event.eventId}/>
-        })
-      }
+      <div>
+        <h1>Events</h1>
+        {this.state.str.map((event, i) => {
+          return (
+            <Jumbotron
+              parentMethod={this.fetchEvents}
+              mainText={event.eventName}
+              subText={event.venue}
+              displayState="true"
+              eventId={event.eventId}
+            />
+          );
+        })}
       </div>
     );
   }
 }
- export default MyEvents;
+export default MyEvents;

11d6ed58c56f0eb9437ed00b2330a5842e50b64b
SHWETA KULKARNI <kulsv1393@gmail.com>
2018-12-08 13:05:37 -0900

    checkbox testing

---
+++ src/common/test.jsx
@@ -0,0 +1,12 @@
+import CheckboxTxtInput from "./CheckboxTxtInput";
+
+class Test extends React.Component {
+		render(){
+    	return (
+      	<div>
+        	<CheckboxTxtInput />
+        </div>
+      )
+    }
+}
+export default Test;
